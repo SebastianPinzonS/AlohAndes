2df556a124a369d09c0d1fcf2f9f8d162a497a2e
a-cordero1 <98660749+a-cordero1@users.noreply.github.com>
2023-05-07 17:53:45 -0500

    RFS Corde funcionando

--- src/main/java/uniandes/isis2304/alohandes/persistencia/SQLCliente.java
+++ src/main/java/uniandes/isis2304/alohandes/persistencia/SQLCliente.java
@@ -66,4 +66,71 @@ class SQLCliente
 		return (List<Cliente>) q.executeList();
 	}
 
+	public List<Object[]> mostrarUsoAlohAndesClientes (PersistenceManager pm)
+	{
+	    String sql = "SELECT id, tipo," +
+			" DIRECCION_HOSTAL_HABITACION_HOSTAL, NUMERO_HABITACION_HABITACION_HOSTAL," + 
+			" DIRECCION_HOTEL_HABITACION_HOTEL, NUMERO_HABITACION_HABITACION_HOTEL, "+
+			" DIRECCION_VIVIENDA_UNIVERSITARIA, NUMERO_APARTAMENTO_VIVIENDA_UNIVERSITARIA, "+
+			" DIRECCION_VIVIENDA_HABITACION, NUMERO_APARTAMENTO_VIVIENDA_HABITACION, "+
+			" DIRECCION_APARTAMENTO, NUMERO_APARTAMENTO, DIRECCION_VIVIENDA_EXPRESS"+
+			" FROM"+
+			" (SELECT clientes.id, clientes.tipo,"+
+			" ofertas.DIRECCION_HOSTAL_HABITACION_HOSTAL, ofertas.NUMERO_HABITACION_HABITACION_HOSTAL, "+
+			" ofertas.DIRECCION_HOTEL_HABITACION_HOTEL, ofertas.NUMERO_HABITACION_HABITACION_HOTEL,"+
+			" ofertas.DIRECCION_VIVIENDA_UNIVERSITARIA, ofertas.NUMERO_APARTAMENTO_VIVIENDA_UNIVERSITARIA,"+
+			" ofertas.DIRECCION_VIVIENDA_HABITACION, ofertas.NUMERO_APARTAMENTO_VIVIENDA_HABITACION,"+
+			" ofertas.DIRECCION_APARTAMENTO, ofertas.NUMERO_APARTAMENTO, ofertas.DIRECCION_VIVIENDA_EXPRESS"+
+			" FROM"+
+			" (SELECT id, "+
+			" NVL(DIRECCION_HOSTAL_HABITACION_HOSTAL,'No Aplica') AS DIRECCION_HOSTAL_HABITACION_HOSTAL, NVL(NUMERO_HABITACION_HABITACION_HOSTAL,'No Aplica') AS NUMERO_HABITACION_HABITACION_HOSTAL, "+
+			" NVL(DIRECCION_HOTEL_HABITACION_HOTEL,'No Aplica') AS DIRECCION_HOTEL_HABITACION_HOTEL, NVL(NUMERO_HABITACION_HABITACION_HOTEL,'No Aplica') AS NUMERO_HABITACION_HABITACION_HOTEL, "+
+			" NVL(DIRECCION_VIVIENDA_UNIVERSITARIA,'No Aplica') AS DIRECCION_VIVIENDA_UNIVERSITARIA, NVL(NUMERO_APARTAMENTO_VIVIENDA_UNIVERSITARIA,'No Aplica') AS NUMERO_APARTAMENTO_VIVIENDA_UNIVERSITARIA, "+
+			" NVL(DIRECCION_VIVIENDA_HABITACION,'No Aplica') AS DIRECCION_VIVIENDA_HABITACION, NVL(NUMERO_APARTAMENTO_VIVIENDA_HABITACION,'No Aplica') AS NUMERO_APARTAMENTO_VIVIENDA_HABITACION, "+
+			" NVL(DIRECCION_APARTAMENTO,'No Aplica') AS DIRECCION_APARTAMENTO, NVL(NUMERO_APARTAMENTO,'No Aplica') AS NUMERO_APARTAMENTO, "+
+			" NVL(DIRECCION_VIVIENDA_EXPRESS,'No Aplica') AS DIRECCION_VIVIENDA_EXPRESS"+
+			" FROM oferta) ofertas"+
+			" LEFT OUTER JOIN"+
+			" (SELECT clientos.id, clientos.nombre, clientos.tipo, reservas.id_oferta"+
+			" FROM"+
+			" (SELECT id_oferta, id_cliente"+
+			" FROM reserva) reservas"+
+			" FULL OUTER JOIN"+
+		 		" (SELECT id, nombre, tipo"+
+				" FROM cliente) clientos"+
+				" ON clientos.id = reservas.id_cliente) clientes"+
+			" ON clientes.id_oferta = ofertas.id"+
+			" ORDER BY clientes.tipo, clientes.id DESC)"+
+			" WHERE id IS NOT NULL";
+		
+	    Query q = pm.newQuery(SQL, sql);
+		return q.executeList();
+	}
+
+	public List<Object[]> mostrarUsoAlohAndesParaUnUsuarioDado (PersistenceManager pm, String idCliente)
+	{
+	    String sql = "SELECT clientes.id_cliente, precio_especial_tomado,"+
+		" (duracion_dias*costo_contrato_dia+costo_adicional_servicios+costo_seguro_arrendamiento) AS precio_total_pagado_normal,"+
+		" (duracion_dias*precio_especial_dia+costo_adicional_servicios+costo_seguro_arrendamiento) AS precio_especial_total_pagado_normal,"+
+		" duracion_dias AS dias_contratados"+
+ 		" FROM"+
+	 	" (SELECT id_cliente, id_oferta, precio_especial_tomado, fecha_realizacion_reserva, fecha_inicial, duracion_dias"+
+	 	" FROM reserva) clientes"+
+	 	" LEFT OUTER JOIN"+
+		" (SELECT id, costo_contrato_dia, precio_especial_dia, costo_adicional_servicios, costo_seguro_arrendamiento,"+
+		" NVL(DIRECCION_HOSTAL_HABITACION_HOSTAL,'No Aplica') AS DIRECCION_HOSTAL_HABITACION_HOSTAL, NVL(NUMERO_HABITACION_HABITACION_HOSTAL,'No Aplica') AS NUMERO_HABITACION_HABITACION_HOSTAL," +
+		" NVL(DIRECCION_HOTEL_HABITACION_HOTEL,'No Aplica') AS DIRECCION_HOTEL_HABITACION_HOTEL, NVL(NUMERO_HABITACION_HABITACION_HOTEL,'No Aplica') AS NUMERO_HABITACION_HABITACION_HOTEL,"+
+		" NVL(DIRECCION_VIVIENDA_UNIVERSITARIA,'No Aplica') AS DIRECCION_VIVIENDA_UNIVERSITARIA, NVL(NUMERO_APARTAMENTO_VIVIENDA_UNIVERSITARIA,'No Aplica') AS NUMERO_APARTAMENTO_VIVIENDA_UNIVERSITARIA, "+
+		" NVL(DIRECCION_VIVIENDA_HABITACION,'No Aplica') AS DIRECCION_VIVIENDA_HABITACION, NVL(NUMERO_APARTAMENTO_VIVIENDA_HABITACION,'No Aplica') AS NUMERO_APARTAMENTO_VIVIENDA_HABITACION, "+
+		" NVL(DIRECCION_APARTAMENTO,'No Aplica') AS DIRECCION_APARTAMENTO, NVL(NUMERO_APARTAMENTO,'No Aplica') AS NUMERO_APARTAMENTO," +
+		" NVL(DIRECCION_VIVIENDA_EXPRESS,'No Aplica') AS DIRECCION_VIVIENDA_EXPRESS"+
+		" FROM oferta) reservas"+
+		" ON clientes.id_oferta = reservas.id"+
+ 		" WHERE clientes.id_cliente = ?";
+		
+	    Query q = pm.newQuery(SQL, sql);
+		q.setParameters(idCliente);
+		return q.executeList();
+	}
+
 }
